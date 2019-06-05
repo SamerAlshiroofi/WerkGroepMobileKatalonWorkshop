918b45ecf6530bf355e8994f58b48883d68e8648
Samer Alshiroofi <samer.alshiroofi@gmail.com>
2019-06-05 14:15:46 +0200

    no message

---
+++ Scripts/Web test/Script1559726287810.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.openBrowser('https://play-accept.primephonic.com/', FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.maximizeWindow()
+
+WebUI.verifyTextPresent('Home', false, FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.verifyTextPresent('Browse', false, FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.verifyTextPresent('My Music', false, FailureHandling.CONTINUE_ON_FAILURE)
+
+'get current url\r\n'
+CurrentUrL = WebUI.getUrl(FailureHandling.CONTINUE_ON_FAILURE)
+
+'match current url with expected url\r\n'
+WebUI.verifyMatch(CurrentUrL, 'https://play-accept.primephonic.com/', false, FailureHandling.CONTINUE_ON_FAILURE)
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/WebTest.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WebTest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ef0211b0-1bbd-4ce6-9484-901848898c9c</testSuiteGuid>
+   <testCaseLink>
+      <guid>f7278bd8-6e7f-4b26-bf6c-49e7107fc07a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
